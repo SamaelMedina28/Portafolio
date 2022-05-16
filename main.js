a66ceead208269ae0ee6cb6701fd41ccfaa17f17
Samael Medina <SAMAEL@Dominio.TJ>
2022-05-16 12:12:36 -0700

    termino principal de diseño

---
+++ main.js
@@ -0,0 +1,7 @@
+const button =  document.getElementById("button")
+const menu =  document.getElementById("menu")
+
+
+button.addEventListener("click", () =>{
+    menu.classList.toggle("hidden")
+})
