bac3e448e4c36c760258cd846eb32392d52067a3
Samael Medina <SAMAEL@Dominio.TJ>
2022-06-01 12:40:39 -0700

    Termino de base version Alfa

--- main.js
+++
@@ -1,7 +0,0 @@
-const button =  document.getElementById("button")
-const menu =  document.getElementById("menu")
-
-
-button.addEventListener("click", () =>{
-    menu.classList.toggle("hidden")
-})
